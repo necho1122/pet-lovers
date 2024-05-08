0567d583a79e99d7db716fa2031d7b3f35ffe58a
Nelson <nelsonubac90@gmail.com>
2024-05-08 18:21:27 -0300

    feat: Add product section to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,31 @@
+import productsData from '../mocks/productsData.json';
+import './Home.css';
+
 function Home() {
 	return (
 		<div>
 			<h1>Pet Lovers</h1>
+			<h2>Aqui estão alguns dos nossos produtos:</h2>
+			<div className='product-section-home'>
+				{productsData.dogs.accesorios.map((product) => (
+					<div
+						key={product.id}
+						className='items-on-home'
+					>
+						<div className='image-product-container'>
+							<img
+								src={product.foto}
+								alt={product.descripcion}
+							/>
+						</div>
+						<h4>{product.descripcion}</h4>
+						<p>R$: {product.precio}</p>
+						<button className='add-to-cart'>
+							Add to cart
+						</button>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
