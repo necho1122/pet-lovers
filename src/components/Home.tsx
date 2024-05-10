7918b434c7571cf812a10cd76a5accf496410a7b
Nelson <nelsonubac90@gmail.com>
2024-05-10 18:44:08 -0300

    add carousels at home and update styles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,31 +1,96 @@
 import productsData from '../mocks/productsData.json';
 import './Home.css';
 
-function Home() {
+import { useState, useEffect } from 'react';
+
+const Carousel = ({ slides }: { slides: any[] }) => {
+	const [currentSlide, setCurrentSlide] = useState(0);
+	const [slidesToShow, setSlidesToShow] = useState(1);
+
+	useEffect(() => {
+		const resizeHandler = () => {
+			const width = window.innerWidth;
+			setSlidesToShow(Math.floor(width / 210));
+		};
+
+		resizeHandler();
+
+		window.addEventListener('resize', resizeHandler);
+
+		return () => {
+			window.removeEventListener('resize', resizeHandler);
+		};
+	}, []);
+
+	const nextSlide = () => {
+		setCurrentSlide((prevSlide) => (prevSlide + slidesToShow) % slides.length);
+	};
+
+	const prevSlide = () => {
+		setCurrentSlide(
+			(prevSlide) => (prevSlide - slidesToShow + slides.length) % slides.length
+		);
+	};
+
 	return (
-		<div>
-			<h1>Pet Lovers</h1>
-			<h2>Aqui estão alguns dos nossos produtos:</h2>
-			<div className='product-section-home'>
-				{productsData.dogs.accesorios.map((product) => (
+		<div className='carousel'>
+			<button
+				className='carousel-button prev'
+				onClick={prevSlide}
+			>
+				&#10094;
+			</button>
+			{slides
+				.slice(currentSlide, currentSlide + slidesToShow)
+				.map((slide, index) => (
 					<div
-						key={product.id}
-						className='items-on-home'
+						key={index}
+						className='slide'
 					>
-						<div className='image-product-container'>
+						<div className='slide-image'>
 							<img
-								src={product.foto}
-								alt={product.descripcion}
+								src={slide.foto}
+								alt='food image'
 							/>
 						</div>
-						<h4>{product.descripcion}</h4>
-						<p>R$: {product.precio}</p>
-						<button className='add-to-cart'>
-							Add to cart
-						</button>
+						<h5>{slide.descripcion}</h5>
+						<p>{`R$${slide.precio}`}</p>
+						<button className='add-to-cart-btn'>Add to cart</button>
 					</div>
 				))}
-			</div>
+			<button
+				className='carousel-button next'
+				onClick={nextSlide}
+			>
+				&#10095;
+			</button>
+		</div>
+	);
+};
+
+function Home() {
+	const slidesDogFood = productsData.dogs.food;
+	const sliceDogAcessories = productsData.dogs.accesorios;
+	const slidesCatFood = productsData.gatos.comida;
+	const sliceCatAcessories = productsData.gatos.accesorios;
+	const sliceBirdsFood = productsData.birds.food;
+	const sliceBirdsAcessories = productsData.birds.accesorios;
+
+	return (
+		<div className='app'>
+			<h1>Pet Lovers</h1>
+			<h3>Comida para cães</h3>
+			<Carousel slides={slidesDogFood} />
+			<h3>Acessórios para cães</h3>
+			<Carousel slides={sliceDogAcessories} />
+			<h3>Comida para gatos</h3>
+			<Carousel slides={slidesCatFood} />
+			<h3>Acessórios para gatos</h3>
+			<Carousel slides={sliceCatAcessories} />
+			<h3>Comida para aves</h3>
+			<Carousel slides={sliceBirdsFood} />
+			<h3>Acessórios para aves</h3>
+			<Carousel slides={sliceBirdsAcessories} />
 		</div>
 	);
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
+import './Layout.css';
 
 function Layout() {
 	return (
